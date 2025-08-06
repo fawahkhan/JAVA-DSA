51d3c72761d7c2c1fb8a036d991480e8e076496e
fawahkhan <khanmohdfawah21@gmail.com>
2025-08-06 12:45:00 +0530

    Pattern another question solved

--- JAVA/PatternsII.java
+++ JAVA/PatternsII.java
@@ -1,3 +1,12 @@
+/*
+
+to print hollow rectangle pattern
+*****
+*   *
+*   *
+*****
+
+
 public class PatternsII {
     public static void SquarePattern(int maxRows , int maxCols){
         for (int i=1 ; i<=maxRows ; i++){
@@ -20,4 +29,62 @@ public class PatternsII {
         SquarePattern(4,5);
     }
     
+}
+ */
+
+
+/*
+to print inverted half star 
+    * 
+   **
+  ***
+ ****
+ */
+/* 
+ public class PatternsII {
+
+    public static void invertAndRotatedPyramid(int maxRows ){
+        for(int i = 1 ; i<=maxRows ; i++){
+            //for spaces
+            for(int j=1 ; j<=maxRows-i ; j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1 ; j<=i ; j++){
+                System.out.print("*");
+            } 
+            System.out.println();
+        }
+    }
+    public static void main (String args[]){
+        invertAndRotatedPyramid(9);
+    }
+}
+*/
+
+
+/*
+inverted half pyramid with numbers
+12345
+1234
+123
+12
+1
+ */
+
+public class PatternsII{
+    public static void invertedHalfPyramid(int maxRows){
+        for(int i=1 ;i<=maxRows; i++ ){
+            for(int num =1 ; num<= maxRows-i+1 ; num++){
+                System.out.print(num);
+            }
+            for(int space = 1 ; space<=i-1 ; space++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        invertedHalfPyramid(5);
+    }
 }
