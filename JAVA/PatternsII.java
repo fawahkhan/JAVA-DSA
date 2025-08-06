2493e2b00704773dce86ae0a7942cd85aec2da5d
fawahkhan <khanmohdfawah21@gmail.com>
2025-08-06 20:56:36 +0530

    Patterns completed

--- JAVA/PatternsII.java
+++ JAVA/PatternsII.java
@@ -71,7 +71,7 @@ inverted half pyramid with numbers
 12
 1
  */
-
+/*
 public class PatternsII{
     public static void invertedHalfPyramid(int maxRows){
         for(int i=1 ;i<=maxRows; i++ ){
@@ -88,3 +88,97 @@ public class PatternsII{
         invertedHalfPyramid(5);
     }
 }
+ */
+
+// floyd's triangle
+// print this pattern
+/* 
+1
+2 3
+4 5 6
+7 8 9 10
+*/ 
+/*
+public class PatternsII{
+    public static void floydsPattern(int maxRows){
+        int num = 1 ;
+        for (int i=1 ; i<=maxRows ; i++){
+            for (int j=1 ; j<=i ; j++){
+                System.out.print(num + " ");
+                num = num+1 ;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        floydsPattern(5);
+    }
+}
+     */
+
+
+/* 
+PRINT THIS PATTERN
+1
+01
+101
+0101
+10101
+SOLUTION-
+public class PatternsII{
+    public static void patternZeroOne(int maxRows ){
+        for (int i=1 ; i<=maxRows ; i++){
+            for (int j=1 ; j<=i ; j++){
+                if((i+j)%2 == 0 ){
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String args[]){
+        patternZeroOne(5);
+    }
+}
+ */
+public class PatternsII{
+    public static void butterfly(int maxRows){
+        for (int i=1 ; i<=maxRows ; i++){
+            //stars
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("*");
+
+            }
+            for(int j= 1 ; j<=2*(maxRows-i) ; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = maxRows; i>=1 ; i--){
+             for(int j = 1 ; j<=i ; j++){
+                System.out.print("*");
+
+            }
+            for(int j= 1 ; j<=2*(maxRows-i) ; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+    }
+    public static void main(String args[]){
+        butterfly(4);
+    }
+}
+
