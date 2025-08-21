8c08efaad0c76a2c66817728b802ec75bc323e88
fawahkhan <khanmohdfawah21@gmail.com>
2025-08-21 07:57:36 +0530

    first commit in arrrays

--- JAVA/Arrays/ArraysCC.java
+++ JAVA/Arrays/ArraysCC.java
@@ -12,8 +12,9 @@ public class ArraysCC {
         // how to take input 
         Scanner sc = new Scanner(System.in) ;
         // int phy ;
-        // phy = sc.nextInt(); 
-
+        // phy = sc.nextInt();
+         
+        System.out.print("Enter the marks of phy chec=m and maths followed by a space");
         marks[0] = sc.nextInt(); //phy
         marks[1] = sc.nextInt(); //chem
         marks[2] = sc.nextInt(); //math
@@ -21,8 +22,26 @@ public class ArraysCC {
         System.out.println("chem" + marks[1]);
         System.out.println("maths" + marks[2]);
 
-    }
     
+    // if we want to add marks in an array 
+    marks[2] = 56 ;// just told the marks that were to be updated
+    marks[1] = marks[1] + 5  ;//another way of editing or redefining whats present in an array
+
+
+    //performing operations on array 
+
+    int percentage = (marks[0]+marks[1]+marks[2])/3 ;
+
+    System.out.println("percentage= " + percentage);
+
+    // to know the length of an array 
+    System.out.println("length of array =" + marks.length);
+
+
+
+
+    }
+
 
 
 }
